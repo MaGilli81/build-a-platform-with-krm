f4ff6f0ccac5ee01f7362418ae6bf18b9b3f5122
Matt Gilliam <50531124+MaGilli81@users.noreply.github.com>
2023-03-07 10:42:36 -0600

    Create initial-setup.sh

---
+++ 1-setup/initial-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+ export PROJECT_ID=$(gcloud config get-value project)
+ export PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format="value(projectNumber)")
+ export GITHUB_USERNAME="MaGilli81"
+
+
+ gcloud config set project ${PROJECT_ID}
+
+ gcloud services enable \
+  cloudresourcemanager.googleapis.com \
+  container.googleapis.com \
+  cloudbuild.googleapis.com \
+  sqladmin.googleapis.com \
+  secretmanager.googleapis.com \
+  cloudasset.googleapis.com \
+  storage.googleapis.com \
+  gkehub.googleapis.com
